2c92bad4a9ad99222e57cb294047a7f5d6adebe4
Dylan Fox <dylan.fox.kc@gmail.com>
2026-01-24 17:31:24 -0600

    fixing error

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
   },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
-export default nextConfig
+export default nextConfig;
